be5bd7eff3657911661674de07bcadad0f48f0ef
Marti Pericay <marti.pericay@gmail.com>
2016-01-14 19:24:27 +0100

    addd text modal, 3 sections and top buttons
    i18n pending

--- static/js/modules/ui.js
+++ static/js/modules/ui.js
@@ -3,11 +3,32 @@
  */
 
 //define(['i18n', 'map', 'bootstrap'], function(i18n) {
-define(['map', 'bootstrap'], function() {
+define(['text!../../sections/about.ca.html', 'text!../../sections/disclaimer.ca.html', 'text!../../sections/help.ca.html','map', 'bootstrap'], function(about_ca, disclaimer_ca, help_ca) {
 	
 	$("#toggle-button").click(function(e) {
 	    e.preventDefault();
 	    $("#wrapper").toggleClass("toggled");
+	});
+	
+	// text modal management
+	var texts = {
+		"about": {
+			"ca": about_ca 
+		},
+		"disclaimer": {
+			"ca": disclaimer_ca 
+		},
+		"help": {
+			"ca": help_ca 
+		}
+	};
+	
+	$(document).on("click", ".open-textModal", function () {
+	     var pageId = $(this).data("id");
+	     var html = texts[pageId]["ca"];
+	     $('#textModal .modal-content').css('height',$( window ).height()*0.8);
+	     $("#textModal .modal-body").html(html);
+	     // it is superfluous to have to manually call the modal.
 	});
 	
 	/*var bundle = {
