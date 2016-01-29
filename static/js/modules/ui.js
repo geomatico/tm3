428012c7a7bae43047d16308873f658a5d365235
Marti Pericay <marti.pericay@gmail.com>
2016-01-29 17:48:41 +0100

    Undo static pages (expo version). Closes #6

--- static/js/modules/ui.js
+++ static/js/modules/ui.js
@@ -2,7 +2,7 @@
  * @author Martí Pericay <marti@pericay.com>
  */
 
-define(['text!../../sections/about.ca.html', 'text!../../sections/disclaimer.ca.html', 'text!../../sections/help.ca.html', 'i18n', 'taxon', 'map', 'bootstrap'], function(about_ca, disclaimer_ca, help_ca, i18n, taxon, map) {
+define(['i18n', 'taxon', 'map', 'bootstrap'], function(i18n, taxon, map) {
     "use strict";
     	
 	var params = {};
@@ -141,26 +141,6 @@ define(['text!../../sections/about.ca.html', 'text!../../sections/disclaimer.ca.
 	$("#toggle-button").click(function(e) {
 	    e.preventDefault();
 	    $("#wrapper").toggleClass("toggled");
-	});
-	
-	// text modal management
-	var texts = {
-		"about": {
-			"ca": about_ca 
-		},
-		"disclaimer": {
-			"ca": disclaimer_ca 
-		},
-		"help": {
-			"ca": help_ca 
-		}
-	};
-	$(document).on("click", ".open-textModal", function () {
-	     var pageId = $(this).data("id");
-	     var html = texts[pageId]["ca"];
-	     $('#textModal .modal-content').css('height',$( window ).height()*0.8);
-	     $("#textModal .modal-body").html(html);
-	     // it is superfluous to have to manually call the modal.
 	});
 	
 	// cannot be used because keys of translations.json are the English ones and we may have changed them ...
