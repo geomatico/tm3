1e7a648f551ca1a846f4248b20ad635ac12b1c57
Marti Pericay <marti.pericay@gmail.com>
2016-06-27 18:45:11 +0200

    remove unnecessary dependencies

--- static/js/config.js
+++ static/js/config.js
@@ -7,8 +7,6 @@ var require = (function() {
 		paths: {
 			"bootstrap": "//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min",
             "leaflet-draw": "//cdnjs.cloudflare.com/ajax/libs/leaflet.draw/0.2.3/leaflet.draw",
-            "leaflet-maskcanvas": LIB_PATH + "leaflet.maskcanvas/src/L.TileLayer.MaskCanvas",
-            "quadtree": LIB_PATH + "leaflet.maskcanvas/src/QuadTree",
             "css": LIB_PATH + "require-css/css",
             "cartodb": "//libs.cartocdn.com/cartodb.js/v3/3.15/cartodb",
             "jquery": "//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min",
@@ -22,9 +20,6 @@ var require = (function() {
 			},
 			"leaflet-draw" : {
 				deps: ["cartodb", "css!//cdnjs.cloudflare.com/ajax/libs/leaflet.draw/0.2.3/leaflet.draw.css"]
-			},
-			"leaflet-maskcanvas" : {
-				deps: ["cartodb", "quadtree"]
 			},
 			"cartodb" : {
 				deps: ["css!//libs.cartocdn.com/cartodb.js/v3/3.15/themes/css/cartodb.css"]
