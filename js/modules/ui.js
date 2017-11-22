c62610ebce6535f49cee92a8a1a674db192fbf64
Marti Pericay <marti.pericay@gmail.com>
2017-11-22 13:21:01 +0100

    Closes #103

--- js/modules/ui.js
+++ js/modules/ui.js
@@ -5,7 +5,10 @@
 define(['i18n', 'taxon', 'map', 'search', 'text!../../sections/help.html', 'text!../../sections/about.ca.html', 'text!../../sections/about.es.html', 'text!../../sections/about.en.html', 'bootstrap', 'typeahead', 'select'], function(i18n, taxon, map, search, help, about_ca, about_es, about_en) {
     "use strict";
     	
-	var params = {};
+	//default language
+    i18n.setLang("ca");
+    
+    var params = {};
     location.search.substr(1).split("&").forEach(function(item) {
         var kv = item.split("=");
         params[kv[0]] = kv[1];
