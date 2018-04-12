b9371c5d0ce5d0b1c63f0d326e20b188b93d5f84
Marti Pericay <marti.pericay@gmail.com>
2018-04-12 18:01:42 +0200

    Build tabs in sheet. Closes #125

--- js/modules/sheet.js
+++ js/modules/sheet.js
@@ -12,67 +12,80 @@ define(['jquery', 'i18n', 'mustache'], function($, i18n) {
 	     
          var title = data.parse.title;
          div.find("#title").html(title);
+         
          var moreinfo = i18n.t("More info");
          var here = i18n.t("here");
          
          div.find("#subtitle").html(moreinfo + " <a href='http://" + localeWiki + '.' + wikiApi + title + "' target='_blank'>" + here + "</a>").show();
 
          // get raw HTML text ... but we need to do a few hacks
-         div.find("#desc").html(data.parse.text["*"]);
+         $("#tabWiki").html(data.parse.text["*"]);
          
          //HACKS
          //remove links
-         $('#desc a').replaceWith(function() {
+         $('#tabWiki a').replaceWith(function() {
              return this.childNodes;
          });
          //remove areas (with links)
-         $('#desc area').remove();         
+         $('#tabWiki area').remove();         
          //remove references
-         $('#desc .reference ').remove();
+         $('#tabWiki .reference ').remove();
          //remove references errors
-         $('#desc .mw-ext-cite-error').remove();
+         $('#tabWiki .mw-ext-cite-error').remove();
          //remove disambiguations 
-         $('#desc .noprint').remove();
+         $('#tabWiki .noprint').remove();
      };
      
      var drawLinksSheet = function(title){
-         var div = $("#links");
-         div.find("#wikispecies").attr("href", "http://species.wikimedia.org/wiki/"+title);
-         div.find("#gbif").attr("href", "http://www.gbif.org/species/search?q="+title);
-         div.find("#eol").attr("href", "http://www.eol.org/search?q="+title.replace(" ","+"));
+        var links = ['<a id="wikispecies" href="http://species.wikimedia.org/wiki/{{title}}" target="_blank"><img alt="Wikispecies Logo" title="Wikispecies" src="img/logos/wikispecies.png" /></a>',
+                '<a id="eol" href="http://www.gbif.org/species/search?q={{title}}" target="_blank"><img alt="Encyclopedia Of Life Logo" title="Encyclopedia Of Life" src="img/logos/eol.png" /></a>',
+                '<a id="gbif" href="http://www.eol.org/search?q={{title2}}" target="_blank"><img alt="GBIF Logo" title="GBIF" src="img/logos/gbif.jpg" /></a>'].join("\n");
+        
+        var data = {
+            "title": title,
+            "title2": title.replace(" ","+")
+        }
+        
+        links = Mustache.render(links, data);
+        
+         $("#tabLinks").html(links);
      };
      
-     var createSheetDiv = function(){
+     var createTabs = function(){
         
         var html= ['<div id="divSheetModal">',
                     '<div id="content">',
-                    '    <div id="title"></div>',
-                    '    <div id="subtitle"></div>',
-                    '    <div id="textColumn">',
-                    '        <div id="desc"></div>',
-                    '    </div>',
-                    ' </div>',
-                    ' <div id="loading" class="text-center">',
-                    '    <img src="img/load.svg" />',
-                    '{{loadingtext}}',
-                    '<br/>',
-                    '    <img alt="Wikipedia Logo" src="img/logos/wiki.png" />',
-                    ' </div>',
+                    '   <div id="title"></div>',
+                    '   <div id="subtitle"></div>',
+                    '   <ul class="nav nav-tabs" id="sheetTabs">',
+                    '       <li class="active"><a href="#" data="tabStats">{{stats}}</a></li>',
+                    '       <li><a href="#" data="tabWiki">{{wikipedia}}</a></li>',
+                    '       <li><a href="#" data="tabLinks">{{links}}</a></li>',
+        			'   </ul>',
+                    '   <div id="loading" class="text-center">',
+                    '       <img src="img/load.svg" />',
+                    '       {{loadingtext}}',
+                    //'       <br/>',
+                    //'       <img alt="Wikipedia Logo" src="img/logos/wiki.png" />',
+                    '   </div>',
+                    '   <div id="tabStats"></div>',
+                    '   <div id="tabWiki"></div>',
+                    '   <div id="tabLinks"></div>',
                     ' </div>'].join("\n");
         
         var data = {
-            loadingtext: i18n.t('Loading from Wikipedia')
+            loadingtext: i18n.t('Loading from Wikipedia'),
+            stats: i18n.t('Stats'),
+            wikipedia: i18n.t('Wikipedia'),
+            links: i18n.t('Links')
         };
         
-        var footer = '<div id="links"><a id="wikispecies" target="_blank"><img alt="Wikispecies Logo" title="Consultar Wikispecies" src="img/logos/wikispecies.png" /></a><a id="eol" target="_blank"><img alt="Encyclopedia Of Life Logo" title="Consultar Encyclopedia Of Life" src="img/logos/eol.png" /></a><a id="gbif" target="_blank"><img alt="GBIF Logo" title="Consultar GBIF" src="img/logos/gbif.jpg" /></a></div>';
-        
-        return Mustache.render(html, data) + footer;
+        return Mustache.render(html, data);
      };
      
      var getWikiInfo = function(div, taxon){
          
          var wiki_url = "http://" + localeWiki + ".wikipedia.org/w/api.php?action=parse&prop=text&section=0&format=json&page="+ taxon + "&contentformat=text%2Fx-wiki&redirects=";
-         div.find("#links").hide();
              
          $.getJSON(wiki_url+"&callback=?", //for JSONP
             {
@@ -81,26 +94,39 @@ define(['jquery', 'i18n', 'mustache'], function($, i18n) {
             },
             function(data){
             // parse JSON data
-                if(data.parse) drawWikiSheet(div, data);
-                else {
+                if(data.parse) {
+                    drawWikiSheet(div, data);
+                    drawLinksSheet(taxon);                    
+                } else {
                     div.find("#title").html("No results");
-                    div.find("#desc").html("No results found for " + " " + taxon + " " + " at Wikipedia");
-                    div.find("#subtitle").hide();
+                    div.find("#subtitle").html("No results found for " + " " + taxon + " " + " at Wikipedia");
+                    div.find("#sheetTabs").hide();
                 }
-                div.find("#content").show();
-                div.find("#links").show();
+                //tab to show
+                div.find("#tabStats").show();
                 div.find("#loading").hide();
-                drawLinksSheet(taxon);
         });
              
-
      };
     
 	return {
        showSheet: function(div, taxon, locale) {
             divWiki = div;
             if(locale) localeWiki = locale;
-            $(div).html(createSheetDiv());
+            $(div).html(createTabs());
+            //tabs
+            $("#tabStats").hide();
+            $("#tabWiki").hide();
+            $("#tabLinks").hide();
+            $('#sheetTabs a').click(function (e) {
+                e.preventDefault();
+                $(this).tab("show");
+                $("#tabWiki").hide();
+                $("#tabStats").hide();
+                $("#tabLinks").hide();
+                var div = $(this).attr("data");
+                $("#" + div).show();
+            });            
 			return getWikiInfo(div, taxon);
        }
 	};
